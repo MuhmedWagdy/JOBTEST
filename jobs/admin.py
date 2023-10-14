9fafe31857b1652e485d6cc274108b868585770d
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-10-14 14:47:33 +0300

    add model job

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+
+from .models import job_catogery,Job_Detail
 # Register your models here.
+
+admin.site.register(job_catogery)
+admin.site.register(Job_Detail)
--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,55 @@
 from django.db import models
+# from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+job_type = (
+    ('Full Time','Full Time'),  
+    ('Part Time','Part Time'),
+    ('Remote','Remote'), 
+    ('Freelance','Freelance'),
+
+)
+
+class job_catogery(models.Model):
+
+    job_type = models.CharField(max_length=200,choices=job_type)
+
+    job_location = models.CharField(max_length=200)
+
+    experience = models.IntegerField()
+
+    posted_within = models.DateTimeField(default=timezone)
+
+
+
+class Job_Detail(models.Model):
+
+    logo = models.ImageField(upload_to="image_logo")
+
+    job_name = models.CharField(max_length=100)
+
+    catogory_job = models.ForeignKey(job_catogery, on_delete=models.CASCADE)
+
+    place = models.CharField(max_length=200)
+
+    salary = models.FloatField(range(3500,4000))
+
+    time_puplish = models.DateTimeField(null=True,blank=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
